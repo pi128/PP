5907506b544c7555367768a74f90f31c7423fa5d
pi128 <77074839+pi128@users.noreply.github.com>
2025-04-27 00:47:10 -0400

    A pdf spliter I made to slice pdfs int .txt every 5 pages

---
+++ pdfSplitter/pdfSplit.py
@@ -0,0 +1,60 @@
+import fitz
+import os
+import glob
+
+downloads_folder = os.path.expanduser("~/Downloads")
+
+files = glob.glob(os.path.join(downloads_folder, "*"))
+
+files.sort(key=os.path.getmtime, reverse=True)
+
+
+# Print the 10 most recent files
+n = 0
+files_dict = {}
+
+for file in files[:10]:
+    print(str(n) + " " + os.path.basename(file))
+    n += 1
+    files_dict[n] = file
+
+
+
+file = input("Enter selection (as num) ")
+
+
+output_folder = "outputs"
+os.makedirs(output_folder, exist_ok=True)
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+file = int(file)
+
+#print(files_dict[file])
+
+
+
+
+doc = fitz.open(files_dict[file])
+i = 0
+page_text = ""
+
+for page_num, page in enumerate(doc, start = 1):
+    page_text += page.get_text()
+
+    if page_num % 5 == 0:
+        filename = os.path.join(output_folder, f"output_{i}.txt")
+        with open(filename, "w") as f:
+            f.write(page_text)
+        page_text = ""
+        i += 1
+
+if page_text:
+    filename = os.path.join(output_folder, f"output_{i}.txt")
+
+    with open(filename, "w") as f:
+        f.write(page_text)
+
+
+doc.close
