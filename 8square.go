c79e2d687f2b96f934326a5687ef5dcb6f09d381
James Widner <77074839+pi128@users.noreply.github.com>
2025-10-27 13:14:23 -0400

    eep

--- 8square.go
+++ 8square.go
@@ -2,24 +2,20 @@ package main
 
 import (
 	"fmt"
-	
-	 "math/rand"
 
+	"math/rand"
 )
 
 func main() {
-	cube := []int{0,1,2,3,4,5,6,7,8}
+	cube := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
 
-	for i := len(cube) - 1; i > 0; i--{
+	for i := len(cube) - 1; i > 0; i-- {
 
-		j := rand.Intn(i + 1) 
+		j := rand.Intn(i + 1)
 
 		cube[i], cube[j] = cube[j], cube[i]
 
 	}
 	fmt.Println(cube)
-
-	
-
 
 }
