dd885ffeeb5f1f431c2edf73a014c9efc9645e0f
James Widner <77074839+pi128@users.noreply.github.com>
2025-11-04 17:05:51 -0500

    Add Arduino MicroPython blink code

---
+++ arduino/blink.py
@@ -0,0 +1,32 @@
+""" 
+   To run this script on your Arduino:
+    source venv/bin/activate
+    mpremote connect /dev/cu.usbmodem101 cp blink.py :
+    mpremote connect /dev/cu.usbmodem101 exec 'import blink'
+    
+"""
+
+
+try:
+    from machine import Pin
+    import time
+
+
+except ImportError:
+
+
+    exit(1)
+
+led = Pin(48, Pin.OUT)
+
+def blink(times=5, delay=0.5):
+    print(f'Blinking LED {times} times...')
+    for i in range(times):
+        led.on()
+        time.sleep(delay)
+        led.off()
+        time.sleep(delay)
+    print('Done!')
+
+if __name__ == '__main__':
+    blink(times=10, delay=0.5)
